052f4346171d77c2e970126ca9333d2878e6469a
devflaco <npressure00@gmail.com>
2023-11-29 21:27:44 -0300

    added animations + javascript

--- js/index.js
+++ js/index.js
@@ -1,15 +1,23 @@
-var swiper = new Swiper(".slide-content", {
-        slidesPerView: 3,
-        spaceBetween:30,
-        slidesPerGroup:3,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-});
+window.addEventListener('mousemove', handleMouseMove);
+window.addEventListener('resize', handleWindowResize);
+
+const spansSlow = document.querySelectorAll('.spanSlow');
+const spansFast = document.querySelectorAll('.spanFast');
+
+let width = window.innerWidth;
+
+function handleMouseMove(e) {
+  let normalizedPosition = e.pageX / (width/2) - 1;
+  let speedSlow = 100 * normalizedPosition;
+  let speedFast = 200 * normalizedPosition;
+  spansSlow.forEach((span) => {
+    span.style.transform = `translate(${speedSlow}px)`;
+  });
+  spansFast.forEach((span) => {
+    span.style.transform = `translate(${speedFast}px)`
+  })
+}
+//we need to recalculate width when the window is resized
+function handleWindowResize() {
+  width = window.innerWidth;
+}
