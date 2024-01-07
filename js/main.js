47e2d534516bf3864fe60837fecf3f10db253b95
devflaco <npressure00@gmail.com>
2024-01-07 17:53:36 -0300

    getStarted.html almost ready

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const main = document.getElementById("mainC");
 
- 
+signUpButton.addEventListener("click", () => {
+  main.classList.add("right-panel-active");
+});
+signInButton.addEventListener("click", () => {
+  main.classList.remove("right-panel-active");
+});
